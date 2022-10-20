f10f562de6d4498ffcc9e62af077cd5a5c22bb3a
asyncio886 <2409298768@qq.com>
2022-10-20 10:26:25 +0800

    修改了一些问题

--- src/code/Menu.ts
+++ src/code/Menu.ts
@@ -1,37 +1,37 @@
 import app from "./AppContainer";
 class Menu {
-    private negativeCallback:()=>void;
-    private postiveCallback:()=>void;
-    private gongdeShow:HTMLDivElement;
+    private negativeCallback: () => void;
+    private postiveCallback: () => void;
+    private gongdeShow: HTMLDivElement;
     private static localKey = "gongde";
-    private loadGongdeLocal():void{
-        let value:string|null = localStorage.getItem(Menu.localKey);
-        if(!value){
+    private loadGongdeLocal(): void {
+        let value: string | null = localStorage.getItem(Menu.localKey);
+        if (!value) {
             this.gongdeCount = 0;
         }
-        else{
+        else {
             this.gongdeCount = (Number.isNaN(value) ? 0 : Number(value));
         }
     }
-    private storeLocal(){
+    private storeLocal() {
         localStorage.setItem(Menu.localKey, this.gongdeCount + "");
     }
     // 每次增加削减的功德
-    private perValue:number = 10;
+    private perValue: number = 10;
     // 功德数
-    private gongdeCount:number = 0;
+    private gongdeCount: number = 0;
     public onPositiveClick(callback: () => void): void {
         this.postiveCallback = callback;
     }
     public onNegativeClick(callback: () => void): void {
         this.negativeCallback = callback;
     }
-    private addGongde():void{
+    private addGongde(): void {
         this.gongdeCount += this.perValue;
         this.gongdeShow.innerText = this.gongdeCount + "";
         this.storeLocal();
     }
-    private removeGongde():void{
+    private removeGongde(): void {
         this.gongdeCount -= this.perValue;
         this.gongdeShow.innerText = this.gongdeCount + "";
         this.storeLocal();
@@ -42,14 +42,14 @@ class Menu {
         container.classList.add("menu-container")
         let positiveBtn = document.createElement("button");
         positiveBtn.innerText = "积攒功德";
-        positiveBtn.onclick = ()=>{
+        positiveBtn.onclick = () => {
             this.postiveCallback();
             this.addGongde();
         }
         let negativeBtn = document.createElement("button");
         negativeBtn.innerText = "销毁功德";
-        negativeBtn.onclick = ()=>{
-            if(this.gongdeCount === 0){
+        negativeBtn.onclick = () => {
+            if (this.gongdeCount === 0) {
                 alert("你还没有功德")
                 return;
             }
